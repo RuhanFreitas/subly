091b103b83d1736d3075d418d9dd9b0dae74fbd5
Ruhan Freitas <ruhanfreitas1@outlook.com>
2026-06-30 14:40:53 -0300

    migrations: create users and subscriptions tables

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,33 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"subly/internal/config"
+	"subly/internal/database"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
+// migrate create -ext sql -dir migrations -seq create_users_table
+// migrate -path migrations -database $env:DATABASE_URL up
+// migrate -path migrations -database "postgresql://appuser:apppassword@localhost:5432/appdb?sslmode=disable" up
+
 func main() {
+	var cfg *config.Config
+	var err error
+	cfg, err = config.Load()
+	if err != nil {
+		log.Fatal("Failed to load configuration")
+	}
+
+	var pool *pgxpool.Pool
+	pool, err = database.Connect(cfg.DatabaseURL)
+	if err != nil {
+		log.Fatal("Failed to connect to database")
+	}
+
 	var router *gin.Engine = gin.Default()
 
 	router.GET("/healthy", func(c *gin.Context) {
@@ -14,6 +35,10 @@ func main() {
 			"Health": "The API is healthy!",
 		})
 	})
+
+	// User
+
+	// Subscription
 
 	router.Run(":8081")
 }
