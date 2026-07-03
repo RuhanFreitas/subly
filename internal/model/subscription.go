721ef66dd0958d3cf2fb4fcc28b16e46e9efbb60
Ruhan Freitas <ruhanfreitas1@outlook.com>
2026-07-03 14:33:22 -0300

    feat: add subscription model

---
+++ internal/model/subscription.go
@@ -0,0 +1,15 @@
+package model
+
+import "time"
+
+type Subscription struct {
+	ID                string    `json:"id" db:"id"`
+	UserID            string    `json:"user_id" db:"user_id"`
+	Name              string    `json:"name" db:"name"`
+	Price             float64   `json:"price" db:"price"`
+	StartingDate      time.Time `json:"starting_date" db:"starting_date"`
+	PaymentDate       time.Time `json:"payment_date" db:"payment_date"`
+	SubscriptionRenew time.Time `json:"subscription_renew" db:"subscription_renew"`
+	CreatedAt         time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt         time.Time `json:"updated_at" db:"updated_at"`
+}
