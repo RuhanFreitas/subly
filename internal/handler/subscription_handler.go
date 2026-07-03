95994b32d9af2e1921882815a049b78fdeea6313
Ruhan Freitas <ruhanfreitas1@outlook.com>
2026-07-03 14:58:01 -0300

    feat(subscription): add create subscription

--- internal/handler/subscription_handler.go
+++ internal/handler/subscription_handler.go
@@ -1,1 +1,54 @@
 package handler
+
+import (
+	"net/http"
+	"subly/internal/model"
+	"subly/internal/repository"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+// The user ID is currently coming from the params, but we need to change that later
+// using the JWT and adding the user id in the request body so they don't need to parse ids
+
+type SubscriptionInput struct {
+	Name              string    `json:"name" binding:"required"`
+	Price             float64   `json:"price" binding:"required"`
+	StartingDate      time.Time `json:"starting_date" binding:"required"`
+	PaymentDate       time.Time `json:"payment_date" binding:"required"`
+	SubscriptionRenew time.Time `json:"subscription_renew" binding:"required"`
+}
+
+func CreateSubscription(pool *pgxpool.Pool) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		id := c.Param("id")
+
+		var input *SubscriptionInput
+
+		err := c.ShouldBindJSON(&input)
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid body request"})
+			return
+		}
+
+		var subscription *model.Subscription = &model.Subscription{
+			UserID:            id,
+			Name:              input.Name,
+			Price:             input.Price,
+			StartingDate:      input.StartingDate,
+			PaymentDate:       input.PaymentDate,
+			SubscriptionRenew: input.SubscriptionRenew,
+		}
+
+		subscription, err = repository.CreateSubscription(pool, subscription)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal server error occuried"})
+			return
+		}
+
+		c.JSON(http.StatusCreated, gin.H{"susbcription": subscription})
+	}
+}
--- internal/repository/subscription_repository.go
+++ internal/repository/subscription_repository.go
@@ -1,1 +1,42 @@
 package repository
+
+import (
+	"context"
+	"subly/internal/model"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func CreateSubscription(pool *pgxpool.Pool, subscription *model.Subscription) (*model.Subscription, error) {
+	var ctx context.Context
+	var cancel context.CancelFunc
+	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	var query string = `
+			INSERT INTO subscriptions (user_id, name, price, starting_date, payment_date, subscription_renew)
+			VALUES ($1, $2, $3, $4, $5, $6)
+			RETURNING id, user_id, name, price, starting_date, payment_date, subscription_renew, created_at, updated_at
+	`
+
+	err := pool.QueryRow(
+		ctx, query, subscription.UserID, subscription.Name, subscription.Price,
+		subscription.StartingDate, subscription.PaymentDate, subscription.SubscriptionRenew,
+	).Scan(
+		&subscription.ID,
+		&subscription.UserID,
+		&subscription.Name,
+		&subscription.Price,
+		&subscription.StartingDate,
+		&subscription.PaymentDate,
+		&subscription.SubscriptionRenew,
+		&subscription.CreatedAt,
+		&subscription.UpdatedAt,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return subscription, nil
+}
